484804e9634fe78b0f6499f03befa3453264030d
Swagatika Behera <swagatikabehera2001@gmail.com>
2022-05-21 15:56:12 +0530

    print n star

---
+++ n_star.cpp
@@ -0,0 +1,15 @@
+// print n stars in same line
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i = 1 ; i <= n ; i++){
+        cout<< "*"<<" ";
+    }
+
+    return 0;
+}
